1abe5c6026dd78c6b4a8f05aa32b99f9494ecce1
cewhitsett <ewhitsett@hmc.edu>
2019-05-01 11:49:34 -0700

    Update app

--- app.py
+++ app.py
@@ -1,31 +1,41 @@
 from flask import Flask, render_template, request, jsonify
 import requests
-from config import weather_key, defaultWeather
+from config import weather_key, defaultWeather, myClasses
 
 app = Flask(__name__)
-weatherUrl = "http://api.openweathermap.org/data/"
+
 values = {
     "lastWeather": defaultWeather
 }
 
+
+@app.route("/getclasses")
+def getclasses():
+    return jsonify(myClasses)
+
 @app.route("/")
 def index():
-    print("buy")
     return render_template("index.html")
 
 
 @app.route("/weather/<lat>/<lon>")
 def weather(lat,lon):
-    url = weatherUrl + "/2.5/weather?lat={}&lon={}&appid={}"
-    r = requests.get(url.format(lat,lon,weather_key))
+    url = "http://api.openweathermap.org/data/"
+    url += "2.5/weather?lat={}&lon={}&appid={}&units={}"
+    url = url.format(lat,lon,weather_key,"imperial")
+
+    r = requests.get(url.format(lat,lon,weather_key,"imperial"))
     data = r.json()
-    if data["cod"] < 200 or data["cod"] > 299:
+
+    if int(data["cod"]) < 200 or int(data["cod"]) > 299:
         print("Error, couldn't get data")
         return jsonify(values["lastWeather"])
     else:
         values["lastWeather"] = data
-    print("Hey")
+
     return jsonify(data)
+
+
 if __name__ == "__main__":
     app.debug = True
     app.run()
